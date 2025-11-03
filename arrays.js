c42f9a8be4e48245d44f4259eb70437c4e0d4ad4
bugsagweler-ctrl <bugsagweler@gmail.com>
2025-11-03 11:22:05 +0200

    JavaScript Exercise 3

---
+++ arrays.js
@@ -0,0 +1,15 @@
+// TODO: Create an empty array called colors 
+let colors=[]
+// TODO: Add three colors to the array using push() 
+colors.push("Blue");
+colors.push("Green");
+colors.push("Black");
+// TODO: Create another array called numbers with five numbers 
+let numbers = [1, 2, 3, 4, 5];
+// TODO: Remove the last color from the colors array 
+colors.pop();
+// TODO: Add a new color to the beginning of the colors array 
+colors.unshift("Yellow");
+// TODO: Print the length of both arrays 
+console.log("Colors array length:", colors.length);
+console.log("Number array length", numbers.length);
